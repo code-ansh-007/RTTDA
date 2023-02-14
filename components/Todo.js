fabc7e075c27926a4cb2b8a34472c76314249fd6
Ansh <anshpradhancareer03@gmail.com>
2023-02-14 21:13:12 +0530

    made some minor changes

--- components/Todo.js
+++ components/Todo.js
@@ -1,23 +1,24 @@
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-  query,
-  QuerySnapshot,
-  serverTimestamp,
-  setDoc,
-  where,
-} from "firebase/firestore";
-import React, { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { deleteDoc, doc, serverTimestamp, setDoc } from "firebase/firestore";
+import React, { useRef, useState } from "react";
 import { db } from "../firebase";
 
 const Todo = ({ todo, id }) => {
   const [edit, setEdit] = useState(false);
   const [edittedTodo, setEdditedTodo] = useState(todo);
+  const inputRef = useRef(null);
+
+  function setWritingCursor() {
+    setEdit(true);
+    // ? setting the writing cursor to the last word of the input tag
+    if (inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.selectionStart = inputRef.current.value.length;
+      inputRef.current.selectionEnd = inputRef.current.value.length;
+    }
+  }
 
   async function editTodo() {
+    // ? logic to make changes to the firestore db
     await setDoc(
       doc(db, "todos", id),
       {
@@ -42,6 +43,7 @@ const Todo = ({ todo, id }) => {
       <div className="flex items-center space-x-2 my-2 border-2 justify-between sm:max-w-2xl border-blue-500 p-2 w-full rounded-md">
         {edit ? (
           <input
+            ref={inputRef}
             type="text"
             id="edit-input"
             value={edittedTodo}
@@ -63,9 +65,7 @@ const Todo = ({ todo, id }) => {
             ></i>
           ) : (
             <i
-              onClick={() => {
-                setEdit(true);
-              }}
+              onClick={setWritingCursor}
               className="fa-solid fa-pencil duration-200 hover:rotate-45 text-orange-400"
             ></i>
           )}{" "}
